2c0c0c38ee943f40aa571f77e6abb917752dce6f
daniellervik <hattibatti@gmail.com>
2010-04-22 13:03:40 +0000

    Made settings.h with global settings for the OS. Sends memory address as argument to main.

--- include/Boot.h
+++ include/Boot.h
@@ -3,12 +3,14 @@
 
 #include "types.h" 
 
+#include "_Boot.h"
 #include "Scheduler.h"
 #include "Process.h"
 #include "IOHandler.h"
 #include "Interrupt.h"
+#include "Settings.h"
 
-int main();
+int OS(int memoryMin);
 
 
 
--- src/Boot.c
+++ src/Boot.c
@@ -1,34 +1,27 @@
 #include "Boot.h"
 
-
 /**
  * main()
  * The main function takes no arguments and is the function that initiate the
  * first essential functions of the OS
  * This function will never reach its end, since run is a non terminating function.
  */
-int main() {
+int OS(int memoryMin) {
 	// Initiate the I/O-devices needed
 	// Module: IOHandler
 	initIO();
 	
 	// Initiate the OS essentials (including the first process/PCB)
 	// Module: Process
-	initOS();
+	initOS(memoryMin);
 	
 	// Enable the interrupt bit and the interrupt handler
 	// Module: Interrupt
 	enableInterrupt();
 	
-	// HERRO WORLD
-	puts("Hello World");
-	
 	// Run the scheduler
 	// Module: Scheduler
-	run();
+	run(memoryMin);
 	
-	
-	
-	
-	return 0;
+	return memoryMin;
 }
