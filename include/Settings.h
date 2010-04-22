71577491cb52c974fc225e85eca0401b6d690c1d
daniellervik <hattibatti@gmail.com>
2010-04-22 13:16:46 +0000

    Heja andhars

--- include/Settings.h
+++ include/Settings.h
@@ -1,11 +1,11 @@
 #ifndef __SETTINGS__
 #define __SETTINGS__
 
-################
-# Settings for the OS
-# PROCESSES are the maxmimum number of processes in the system at the same time
-# PROCESSES_MEMORY_SIZE is the total size of the PCB array we are using as memory
-################
+// ###############
+// Settings for the OS
+// PROCESSES are the maxmimum number of processes in the system at the same time
+// PROCESSES_MEMORY_SIZE is the total size of the PCB array we are using as memory
+// ###############
 
 #define PROCESSES 1
 #define MEMORY_SIZE 524288
