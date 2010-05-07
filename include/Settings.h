02b79bea48c83e61b8ab6480d939f47ff926c275
daniellervik <hattibatti@gmail.com>
2010-05-07 09:54:48 +0000

    20000 processes, 200 queues - Have larger memory size alloc for stack now...

--- include/Settings.h
+++ include/Settings.h
@@ -7,11 +7,11 @@
 // PROCESSES_MEMORY_SIZE is the total size of the PCB array we are using as memory
 // ###############
 
-#define PROCESSES 600
-#define MEMORY_SIZE 524288
+#define PROCESSES 20000
+#define MEMORY_SIZE 4000000
 
 
 // Number of priorities in the system
-#define PRIORITIES 10
+#define PRIORITIES 200
 
 #endif
